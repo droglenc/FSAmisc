27a59c4a705caf39554533dffaadc0ff9db630d2
droglenc <dogle@northland.edu>
2015-07-03 08:15:07 -0500

    Initial population of package.

---
+++ NEWS.md
@@ -0,0 +1,2 @@
+# FSA 0.0.1 ongoing
+* Initial population.  Brought several functions and data sets from `FSA` v0.6.25.
---
+++ R/FSAmisc.R
@@ -0,0 +1,22 @@
+#' @title Miscellaneous functions for simple fisheries stock assessment methods.
+#'
+#' @description In progress or miscellaneous functions eventually destined for the FSA package.
+#'
+#' @details In progress or miscellaneous functions eventually destined for the FSA package.
+#'
+#' Questions, comments, or suggestions should be given on the \href{https://github.com/droglenc/FSAmisc/issues}{GitHub FSAmisc Issues page}.
+#'
+#' Packages with related functionality by the same author are
+#' \itemize{
+#'   \item The \href{http://fishr.wordpress.com/fsa/fsa/}{FSA package} contains more evolved functions.
+#'   \item The \href{http://fishr.wordpress.com/fsa/fsadata/}{FSAdata package} contains additional data sets.
+#'   \item The \href{https://github.com/droglenc/FSAsim}{FSAsim package} simulation routines for various fisheries methods.
+#'   \item The \href{https://github.com/droglenc/FSAWs}{FSAWs package} contains functions for developing and validating standard weight equations.
+#' }
+#'
+#' @docType package
+#'
+#' @name FSAmisc
+#'
+#' @import FSA
+NULL
